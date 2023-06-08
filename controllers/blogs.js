96873609a846e0a5b3435538c5ea36f5d4041caa
Ville Laitinen <ville.o.laitinen@edu.turkuamk.fi>
2023-06-08 03:17:08 +0300

    13.13 complete

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const { Op } = require('sequelize')
 
 const jwt = require('jsonwebtoken')
 const { SECRET } = require('../util/config')
@@ -21,12 +22,21 @@ const tokenExtractor = (req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
+  const where = {}
+
+  if (req.query.search) {
+    where.title = {
+      [Op.iLike]: `%${req.query.search}%`
+    }
+  }
+
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
     include: {
       model: User,
       attributes: ['name']
-    }
+    },
+    where
   })
   res.json(blogs)
 })
