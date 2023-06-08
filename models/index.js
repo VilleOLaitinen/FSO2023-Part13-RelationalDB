ef6cabf4fc3c7e14c8b45d0fc01b80b1f33a5d93
Ville Laitinen <vlaitine@dc.lab>
2023-06-08 12:38:13 +0300

    13.19 complete

--- models/index.js
+++ models/index.js
@@ -1,9 +1,13 @@
 const Blog = require('./blog')
 const User = require('./user')
+const ReadingList = require('./reading_list')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+Blog.belongsToMany(User, { through: ReadingList })
+User.belongsToMany(Blog, { through: ReadingList })
+
 module.exports = {
-  Blog, User
+  Blog, User, ReadingList
 }
